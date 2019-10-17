bfb125b3bd60c90d59baa0438ce7d335e62ffb02
Chris Juber <cjuber5@gmail.com>
2019-10-17 11:49:50 -0600

    changed tab icon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/icon.png',
   pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Chris Juber',
   subHeading: 'Full Stack Web Developer',
